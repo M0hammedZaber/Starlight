8343da38804656979eee8f988bedd08ac35c92be
Nulula <nulula@gmail.com>
2023-02-02 21:02:59 +0000

    added sunrise and sunset logic to logic.js

--- assets/geolocation.js
+++ assets/geolocation.js
@@ -1,1 +1,34 @@
-//geolocation logic
+var bGeolocation = $("#getGeolocation");
+
+var locationData = (function(){
+    var location = {};
+
+    $(bGeolocation).on('click', function(event){
+        event.preventDefault();
+
+        let getLocation = new Promise (function(resolve,reject) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                resolve(position.coords);
+            }, function(error) {
+                reject(error);
+            });
+            });
+
+        getLocation.then(function(coords) {
+            let lat = coords.latitude;
+            let lon = coords.longitude;
+            location = {lat,lon};
+        }) .catch (function(error){
+            console.log('upsy')
+        });
+});
+
+    return {
+        location: location
+    };
+
+})();
+
+setTimeout(function(){
+    console.log(locationData.location);
+    }, 10000);

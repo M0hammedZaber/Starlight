d57f33e36cf6875cd2f83fefe2c55c76f1f200e1
Nulula <117024716+Nulula@users.noreply.github.com>
2023-02-03 15:39:35 +0000

    Revert "Feature/geolocation"

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,1 @@
 // overall website logic
-
-//sunset-sunrise logic
-var lat=36.6754; //this will be changed later to Geolocation API feedback
-var lon=-6.5463; //this will be changed later to Geolocation API feedback
-var queryURL="https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}) .then(function(response){
-    var sunrise = response.results.sunrise;
-    var sunset = response.results.sunset;
-    $("i.fa-sun").text(" "+sunrise); // this will attach the text to icon, looks quite nice
-    $("i.fa-moon").text(" "+sunset); // this will attach the text to icon, looks quite nice
-});
